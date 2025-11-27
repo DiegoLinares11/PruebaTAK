c076e038c222ade93e7ca30f161055cec3aabcc7
DiegoLinares11 <lin221256@uvg.edu.gt>
2025-11-26 19:56:17 -0600

    Feat: Implement JWT Auth logic and ValidarToken endpoint

---
+++ Backend/BackendApi/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        //  Método GET que devuelva un token (10 mins de vida)
+        [HttpGet("GenerarToken")]
+        public IActionResult GenerarToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Creamos los "Claims" (datos del carnet)
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "UsuarioPrueba"),
+                new Claim(ClaimTypes.Email, "prueba@tak.com")
+            };
+
+            // Creamos el token
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(10), // Vence en 10 minutos
+                signingCredentials: credentials);
+
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+
+        //  Método que valide si el token sigue vigente
+        // [Authorize] Solo entra quien tenga token válido.
+        [HttpGet("ValidarToken")]
+        [Authorize]
+        public IActionResult ValidarToken()
+        {
+            return Ok("El token es válido y estás autorizado. ✅");
+        }
+    }
+}
--- Backend/BackendApi/Program.cs
+++ Backend/BackendApi/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using BackendApi.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,22 @@ builder.Services.AddCors(options =>
            .AllowAnyMethod();
     });
 });
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
 // Add services to the container.
 
@@ -37,6 +56,7 @@ app.UseHttpsRedirection();
 
 app.UseCors("NuevaPolitica");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
